ebe24a2eafbca5f228646efcd487e24d22ea5c1a
Pablo Manchon <pablomanchon97@gmail.com>
2023-07-06 18:46:36 -0300

    Agrreglo problemas de back

--- RopaReactSpring/Back/ropavelazco/src/main/java/com/ropa/ropavelazco/controladores/ControladorPrenda.java
+++ RopaReactSpring/Back/ropavelazco/src/main/java/com/ropa/ropavelazco/controladores/ControladorPrenda.java
@@ -2,15 +2,12 @@ package com.ropa.ropavelazco.controladores;
 
 import com.ropa.ropavelazco.entidades.Prenda;
 import com.ropa.ropavelazco.repositorios.RepositorioPrenda;
-import com.ropa.ropavelazco.servicios.ServicioPrenda;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @RestController
 @CrossOrigin("http://localhost:5173")
@@ -23,7 +20,7 @@ public class ControladorPrenda {
     public List<Prenda> ListarPrendas() {
         return repositorioPrenda.findAll();
     }
-    
+
     @PostMapping("/crear")
     Prenda crearPrenda(@RequestBody Prenda prenda) {
         return repositorioPrenda.save(prenda);
@@ -31,8 +28,7 @@ public class ControladorPrenda {
 
     @GetMapping("/{id}")
     public Prenda mostrarPrenda(@PathVariable Long id, ModelMap modelo) {
-        Optional<Prenda> resPrenda = repositorioPrenda.findById(id);
-        return resPrenda.orElse(null);
+        return repositorioPrenda.findById(id).orElse(null);
     }
 }
 
