60388af9ffe50c49768f6374bba43a61b2b48c6c
Pablo Manchon <pablomanchon97@gmail.com>
2023-07-11 17:51:16 -0300

    Aux Cambio rama

--- RopaReactSpring/Frontend/ropaVelasco/src/components/public/Section.jsx
+++ RopaReactSpring/Frontend/ropaVelasco/src/components/public/Section.jsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import { useInView } from "framer-motion";
-import "../../styles.css";
 
 // eslint-disable-next-line react/prop-types
 function Section({ children }) {
